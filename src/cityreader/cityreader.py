48a79b590119ad631514d7b0773addab0e7fe143
seanaleid <seanaleid@gmail.com>
2020-07-10 12:02:46 -0400

    Make smaller practice csv file

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,17 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  
+  def __repr__(self):
+    print(f"\n\n\tcity: {self.name}\n\tlat: {self.lat}\n\tlon: {self.lon}\n\n")
+
+### test runs properly
+# test = City("Washington District of Columbia",38.9047,-77.0163)
+# print(test)
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,14 +32,15 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+    path = 
     
     return cities
 
-cityreader(cities)
+# cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-    print(c)
+# for c in cities:
+#     print(c)
 
 # STRETCH GOAL!
 #
